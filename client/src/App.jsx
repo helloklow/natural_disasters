dd99dd5badc2e5c71f9d1d68b6b1788c6cb078d5
Kelsey Low <helloklow@gmail.com>
2025-01-24 17:28:37 +0000

    test png

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
 import { Paper } from "@mui/material";
-import logo from './logo-transparent-svg.svg';
+import logo from './logo-transparent-png.png';
 
 function App() {
   const URL = "https://natural-disaster-predictor-production.up.railway.app/";
